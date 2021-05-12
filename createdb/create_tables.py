74d1dad94419d1e72fd4c2afb4f0d17f46a19ffa
umayanga <umayangag@gmail.com>
2021-05-12 20:16:22 +0530

    import performance data

--- createdb/create_tables.py
+++ createdb/create_tables.py
@@ -7,6 +7,16 @@ create_match_details = "CREATE  TABLE match_details (id INT NOT NULL AUTO_INCREM
 create_opposition = "CREATE  TABLE opposition (id INT NOT NULL AUTO_INCREMENT, opposition_name VARCHAR(100), PRIMARY KEY (id))"
 create_venue = "CREATE  TABLE venue (id INT NOT NULL AUTO_INCREMENT, venue_name VARCHAR(100), PRIMARY KEY (id))"
 create_season = "CREATE  TABLE season (id INT NOT NULL AUTO_INCREMENT, season_name VARCHAR(100), PRIMARY KEY (id))"
+create_player = "CREATE  TABLE player (id INT NOT NULL AUTO_INCREMENT, player_name VARCHAR(250), PRIMARY KEY (id))"
+create_weather_data = "CREATE  TABLE weather_data (id INT NOT NULL AUTO_INCREMENT, match_id INT, session VARCHAR(100), temp INT, feels INT, " \
+                      "wind INT, gust INT, rain INT, humidity INT, cloud INT, pressure INT, viscosity VARCHAR(100), " \
+                      "PRIMARY KEY (id))"
+create_batting_data = "CREATE  TABLE batting_data (id INT NOT NULL AUTO_INCREMENT, match_id INT, player_id INT, " \
+                      "description VARCHAR(250), runs INT, balls INT, minutes INT, fours INT, sixes INT, strike_rate float, " \
+                      "batting_position INT, PRIMARY KEY (id))"
+create_bowling_data = "CREATE  TABLE bowling_data (id INT NOT NULL AUTO_INCREMENT, match_id INT, player_id INT, " \
+                      "overs FLOAT, balls INT, maidens INT , runs INT, wickets INT, dots INT, fours INT, sixes INT, econ float, " \
+                      "wides INT, no_balls INT, PRIMARY KEY (id))"
 
 db_connection = mysql.connector.connect(
     host="localhost",
@@ -16,7 +26,11 @@ db_connection = mysql.connector.connect(
     database="cricket_data"
 )
 db_cursor = db_connection.cursor()
-db_cursor.execute(create_match_details)
-db_cursor.execute(create_opposition)
-db_cursor.execute(create_venue)
-db_cursor.execute(create_season)
+# db_cursor.execute(create_match_details)
+# db_cursor.execute(create_opposition)
+# db_cursor.execute(create_venue)
+# db_cursor.execute(create_season)
+# db_cursor.execute(create_player)
+# db_cursor.execute(create_weather_data)
+# db_cursor.execute(create_batting_data)
+db_cursor.execute(create_bowling_data)
--- createdb/import_match_details.py
+++ createdb/import_match_details.py
@@ -36,49 +36,156 @@ def get_record_id(table, value):
     return records[0][0]
 
 
-with open('data/match_details.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'{", ".join(row)}')
-        else:
-            opposition = row[8]
-            oppositionId = get_record_id("opposition", opposition)
+def import_match_details():
+    with open('data/match_details.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'{", ".join(row)}')
+            else:
+                opposition = row[8]
+                oppositionId = get_record_id("opposition", opposition)
 
-            venue = row[14]
-            venueId = get_record_id("venue", venue)
+                venue = row[14]
+                venueId = get_record_id("venue", venue)
 
-            season = row[18]
-            seasonId = get_record_id("season", season)
+                season = row[18]
+                seasonId = get_record_id("season", season)
 
-            toss = 0
-            if row[17] == "TRUE":
-                toss = 1
+                toss = 0
+                if row[17] == "TRUE":
+                    toss = 1
 
-            match_number = row[19].replace("ODI No. ", "", 1)
+                match_number = row[19].replace("ODI No. ", "", 1)
 
-            print(line_count)
-            db_cursor.execute(f'INSERT INTO match_details SET'
-                              f' score={int_or_default(row[0])},'
-                              f' wickets={int_or_default(row[1])},'
-                              f' overs={float_or_default(row[2])},'
-                              f' balls={int_or_default(row[3])},'
-                              f' rpo={float_or_default(row[4])},'
-                              f' target={int_or_default(row[5])},'
-                              f' inning={int_or_default(row[6])},'
-                              f' result="{row[7]}",'
-                              f' opposition_id={oppositionId},'
-                              f' date="{row[9]}",'
-                              f' match_id={int(row[10])},'
-                              f' batting_session="{row[12]}",'
-                              f' bowling_session="{row[13]}",'
-                              f' venue_id="{venueId}",'
-                              f' extras={int_or_default(row[16])},'
-                              f' toss={toss},'
-                              f' season_id={seasonId}, '
-                              f' match_number={int(match_number)}'
-                              f'')
-        line_count += 1
-    db_connection.commit()
-    print(f'Processed {line_count} lines.')
+                print(line_count)
+                db_cursor.execute(f'INSERT INTO match_details SET'
+                                  f' score={int_or_default(row[0])},'
+                                  f' wickets={int_or_default(row[1])},'
+                                  f' overs={float_or_default(row[2])},'
+                                  f' balls={int_or_default(row[3])},'
+                                  f' rpo={float_or_default(row[4])},'
+                                  f' target={int_or_default(row[5])},'
+                                  f' inning={int_or_default(row[6])},'
+                                  f' result="{row[7]}",'
+                                  f' opposition_id={oppositionId},'
+                                  f' date="{row[9]}",'
+                                  f' match_id={int(row[10])},'
+                                  f' batting_session="{row[12]}",'
+                                  f' bowling_session="{row[13]}",'
+                                  f' venue_id="{venueId}",'
+                                  f' extras={int_or_default(row[16])},'
+                                  f' toss={toss},'
+                                  f' season_id={seasonId}, '
+                                  f' match_number={int(match_number)}'
+                                  f'')
+            line_count += 1
+        db_connection.commit()
+        print(f'Processed {line_count} lines.')
+
+
+def import_weather_data(filename, session):
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'{", ".join(row)}')
+            else:
+
+                temp = row[5].replace("°c", "", 1)
+                feels = row[6].replace("°c", "", 1)
+                wind = row[7].split(" ", 1)[0]
+                gust = row[8].split(" ", 1)[0]
+                rain = row[9].split(" ", 1)[0]
+                humidity = row[10].replace("%", "", 1)
+                cloud = row[11].replace("%", "", 1)
+                pressure = row[12].split(" ", 1)[0]
+                viscosity = row[13].replace("°c", "", 1)
+
+                print(line_count)
+                db_cursor.execute(f'INSERT INTO weather_data SET'
+                                  f' match_id={int(row[1])},'
+                                  f' session="{session}",'
+                                  f' temp={int_or_default(temp)},'
+                                  f' feels={int_or_default(feels)},'
+                                  f' wind={int_or_default(wind)},'
+                                  f' gust={int_or_default(gust)},'
+                                  f' rain={float_or_default(rain)},'
+                                  f' humidity={int_or_default(humidity)},'
+                                  f' cloud={int_or_default(cloud)},'
+                                  f' pressure={int_or_default(pressure)},'
+                                  f' viscosity="{viscosity}"'
+                                  f'')
+            line_count += 1
+        db_connection.commit()
+        print(f'Processed {line_count} lines.')
+
+
+def import_batting_data():
+    with open('data/batting_data.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'{", ".join(row)}')
+            else:
+                player = row[0]
+                playerId = get_record_id("player", player)
+
+                print(line_count)
+                db_cursor.execute(f'INSERT INTO batting_data SET'
+                                  f' player_id={int(playerId)},'
+                                  f' description="{row[1]}",'
+                                  f' runs={int(row[2])},'
+                                  f' balls={int(row[3])},'
+                                  f' minutes={int_or_default(row[4])},'
+                                  f' fours={int(row[5])},'
+                                  f' sixes={int(row[6])},'
+                                  f' strike_rate={float(row[7])},'
+                                  f' batting_position={int(row[8])},'
+                                  f' match_id={int(row[9])}'
+                                  f'')
+            line_count += 1
+        db_connection.commit()
+        print(f'Processed {line_count} lines.')
+
+
+def import_bowling_data():
+    with open('data/bowling_data.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                print(f'{", ".join(row)}')
+            else:
+                player = row[0]
+                playerId = get_record_id("player", player)
+
+                print(line_count)
+                db_cursor.execute(f'INSERT INTO bowling_data SET'
+                                  f' player_id={int(playerId)},'
+                                  f' overs={float(row[1])},'
+                                  f' balls={int(row[2])},'
+                                  f' maidens={int(row[3])},'
+                                  f' runs={int(row[4])},'
+                                  f' wickets={int(row[5])},'
+                                  f' econ={float(row[6])},'
+                                  f' dots={int(row[7])},'
+                                  f' fours={int(row[8])},'
+                                  f' sixes={int(row[9])},'
+                                  f' wides={int(row[10])},'
+                                  f' no_balls={int(row[11])},'
+                                  f' match_id={int(row[12])}'
+                                  f'')
+            line_count += 1
+        db_connection.commit()
+        print(f'Processed {line_count} lines.')
+
+
+# import_match_details()
+# import_weather_data('data/weather_data-batting.csv', "batting")
+# import_weather_data('data/weather_data-bowling.csv', "bowling")
+# import_batting_data()
+# import_bowling_data()
