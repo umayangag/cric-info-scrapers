9a386ba9c66596ef088add799a918438c16f948e
umayangag <umayangag@gmail.com>
2021-05-24 18:45:11 +0530

    player consistency calculations

---
+++ src/createdb/calculate_batting_consistency.py
@@ -0,0 +1,33 @@
+from config.mysql import get_db_connection
+import pandas as pd
+
+
+def calculate_batting_consistency(db_connection):
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("SELECT id, player_name FROM player")
+    players_list = db_cursor.fetchall()
+
+    for player in players_list:
+        db_cursor.execute(
+            f'SELECT description, runs, balls, minutes, fours, sixes, strike_rate FROM batting_data where player_id={player[0]}')
+        player_data = db_cursor.fetchall()
+        inning_count = len(player_data)
+        if inning_count > 5:
+            df = pd.DataFrame(player_data)
+            not_out_count = len(df[df[0] == "not out"])
+            sum_of_runs = df[1].sum()
+            average_strike_rate = df[6].mean()
+            average_score = sum_of_runs / (inning_count - not_out_count)
+
+            centuries = len(df[df[1] >= 100])
+            fifties = len(df[df[1] >= 50]) - centuries
+            zeros = len(df[df[1] == 0])
+
+            consistency = 0.4262 * average_score + 0.2566 * inning_count + 0.1510 * average_strike_rate + 0.0787 * centuries + 0.0556 * fifties - 0.0328 * zeros
+            print(player[1], consistency)
+            db_cursor.execute(f'UPDATE player SET batting_consistency = {consistency} WHERE player.id = {player[0]}')
+    db_connection.commit()
+
+
+db_connection = get_db_connection()
+calculate_batting_consistency(db_connection)
---
+++ src/createdb/calculate_bowling_consistency.py
@@ -0,0 +1,29 @@
+from config.mysql import get_db_connection
+import pandas as pd
+
+
+def calculate_bowling_consistency(db_connection):
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("SELECT id, player_name FROM player")
+    players_list = db_cursor.fetchall()
+
+    for player in players_list:
+        db_cursor.execute(
+            f'SELECT balls, runs, wickets, econ FROM bowling_data where player_id={player[0]}')
+        player_data = db_cursor.fetchall()
+        inning_count = len(player_data)
+        if inning_count > 5:
+            df = pd.DataFrame(player_data)
+            total_overs = df[0].sum() / 6
+            strike_rate = df[1].sum() / df[2].sum()
+            average = df[1].sum()*6 / df[0].sum()
+            ff = len(df[df[2] >= 5])
+
+            consistency = 0.4174 * total_overs + 0.2634 * inning_count + 0.1602 * strike_rate + 0.0975 * average + 0.0615 * ff
+            print(player[1], consistency)
+            db_cursor.execute(f'UPDATE player SET bowling_consistency = {consistency} WHERE player.id = {player[0]}')
+    db_connection.commit()
+
+
+db_connection = get_db_connection()
+calculate_bowling_consistency(db_connection)
