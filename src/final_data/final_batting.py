444178de4b47d862cbbaafd049f0d0e8d6446765
umayangag <umayangag@gmail.com>
2021-05-25 16:41:43 +0530

    final dataset encoded

--- src/final_data/final_batting.py
+++ src/final_data/final_batting.py
@@ -4,9 +4,9 @@ from final_data.queries import batting_dataset_query
 import os
 
 columns = [
-    "match_id",
     "runs",
     "strike_rate",
+    "match_id",
     "batting_position",
     "player_name",
     "player_consistency",
@@ -28,6 +28,23 @@ columns = [
 
 dirname = os.path.dirname(__file__)
 output_file = os.path.join(dirname, "output\\batting.csv")
+output_file_encoded = os.path.join(dirname, "output\\batting_encoded.csv")
+
+
+def encode_session(value):
+    if value == "day":
+        return 0
+    return 1
+
+
+def encode_viscosity(value):
+    if value == "Excellent":
+        return 3
+    if value == "Good":
+        return 2
+    if value == "Average":
+        return 1
+    return 0
 
 
 def final_batting_dataset(conn):
@@ -35,7 +52,14 @@ def final_batting_dataset(conn):
     db_cursor.execute(batting_dataset_query)
     data_list = db_cursor.fetchall()
     df = pd.DataFrame(data_list, columns=columns)
-    df.to_csv(output_file, index=False)
+
+    df_encoded = df.copy()
+    df_encoded["batting_session"] = df_encoded["batting_session"].apply(encode_session)
+    df_encoded["viscosity"] = df_encoded["viscosity"].apply(encode_viscosity)
+    df_encoded = df_encoded.loc[:, df_encoded.columns != "player_name"]
+
+    df_encoded.to_csv(output_file, index=False)
+    df_encoded.to_csv(output_file_encoded, index=False)
     print(batting_dataset_query)
 
 
