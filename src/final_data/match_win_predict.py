8455c39804dc9a1b5da5db0e4531ab09eef6b2c3
umayangag <umayangag@gmail.com>
2021-06-06 14:00:13 +0530

    win prediction optimized

--- src/final_data/match_win_predict.py
+++ src/final_data/match_win_predict.py
@@ -7,10 +7,12 @@ from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import cross_val_score
 from sklearn import preprocessing
 from sklearn.ensemble import GradientBoostingClassifier
+import matplotlib.pyplot as plt
+import numpy as np
 
 clf = MLPClassifier(solver='sgd', activation='tanh', alpha=1e-5, hidden_layer_sizes=(43, 11, 1), random_state=1,
                     max_iter=10000)
-gb = GradientBoostingClassifier(n_estimators=500)
+gb = GradientBoostingClassifier(n_estimators=1000)
 predictor = gb
 
 dirname = os.path.dirname(__file__)
@@ -18,51 +20,51 @@ dataset_source = os.path.join(dirname, "..\\team_selection\\final_dataset.csv")
 
 input_data = pd.read_csv(dataset_source)
 X = input_data[[
-    'batting_consistency',
-    'batting_form',
-    'batting_temp',
+    # 'batting_consistency',
+    # 'batting_form',
+    # 'batting_temp',
     'batting_wind',
     'batting_rain',
-    'batting_humidity',
+    # 'batting_humidity',
     'batting_cloud',
     'batting_pressure',
     'batting_viscosity',
     'batting_inning',
-    'batting_session',
-    'toss',
-    'venue',
-    'opposition',
-    'season',
-    'runs_scored',
-    'balls_faced',
-    'fours_scored',
-    'sixes_scored',
-    'batting_position',
-    'batting_contribution',
-    'strike_rate',
+    # 'batting_session',
+    # 'toss',
+    # 'venue',
+    # 'opposition',
+    # 'season',
+    # 'runs_scored',
+    # 'balls_faced',
+    # 'fours_scored',
+    # 'sixes_scored',
+    # 'batting_position',
+    # 'batting_contribution',
+    # 'strike_rate',
     'total_score',
     'total_wickets',
     'total_balls',
     'target',
     'extras',
-    'match_number',
-    'bowling_consistency',
-    'bowling_form',
+    # 'match_number',
+    # 'bowling_consistency',
+    # 'bowling_form',
     'bowling_temp',
-    'bowling_wind',
-    'bowling_rain',
+    # 'bowling_wind',
+    # 'bowling_rain',
     'bowling_humidity',
     'bowling_cloud',
-    'bowling_pressure',
-    'bowling_viscosity',
-    'bowling_session',
-    'bowling_venue',
-    'bowling_opposition',
-    'runs_conceded',
-    'deliveries',
-    'wickets_taken',
-    'bowling_contribution',
-    "econ"
+    # 'bowling_pressure',
+    # 'bowling_viscosity',
+    # 'bowling_session',
+    # 'bowling_venue',
+    # 'bowling_opposition',
+    # 'runs_conceded',
+    # 'deliveries',
+    # 'wickets_taken',
+    # 'bowling_contribution',
+    # "econ"
 ]]
 # TODO do not scale toss, result, wickets match number, batting position, etc
 print(len(X.columns))
@@ -97,20 +99,24 @@ def batting_predict(predictor):
     # for classifiers
     # print("Score:", predictor.score(X_test, y_test))
     accuracy = metrics.accuracy_score(y_test, y_pred)
+    cvs = cross_val_score(predictor, X, y, scoring='accuracy', cv=10).mean()
     print("Accuracy:", accuracy)
-    # print("Cross Validation Score:", cross_val_score(predictor, X, y, scoring='accuracy', cv=10).mean())
+    print("Cross Validation Score:", cvs)
     print(confusion_matrix(y_test, y_pred, labels=[0, 1]))
 
-    # plt.figure(figsize=(6 * 1.618, 6))
-    # index = np.arange(len(X.columns))
-    # bar_width = 0.35
-    # plt.barh(index, predictor.feature_importances_, color='black', alpha=0.5)
-    # plt.ylabel('features')
-    # plt.xlabel('importance')
-    # plt.title('Feature importance')
-    # plt.yticks(index, X.columns)
-    # plt.tight_layout()
-    # plt.show()
+    # plt.bar(range(X_train.shape[1]), gb.feature_importances_)
+    # plt.xticks(range(X_train.shape[1]), X.columns)
+
+    plt.figure(figsize=(6 * 1.618, 6))
+    index = np.arange(len(X.columns))
+    bar_width = 0.35
+    plt.barh(index, predictor.feature_importances_, color='black', alpha=0.5)
+    plt.ylabel('features')
+    plt.xlabel('importance')
+    plt.title('Feature importance: Accuracy:' + str(cvs))
+    plt.yticks(index, X.columns)
+    plt.tight_layout()
+    plt.show()
     # print(predictor.coefs_)
     # print(predictor.get_params())
     return accuracy
