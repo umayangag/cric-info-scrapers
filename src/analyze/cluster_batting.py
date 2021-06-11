8219d16120bf6df44042076de88f3a15288b26c7
umayangag <umayangag@gmail.com>
2021-06-11 13:42:16 +0530

    cluster batting

--- src/analyze/cluster_batting.py
+++ src/analyze/cluster_batting.py
@@ -10,9 +10,10 @@ import os
 # find_k = True
 find_k = False
 
-attribute="batting_position"
+
 def cluster_batting_performance(dataset):
-    kmeanModel = KMeans(n_clusters=4)
+    dataset = dataset.copy()
+    kmeanModel = KMeans(n_clusters=6)
     kmeanModel.fit(dataset)
 
     dataset['batting_performance'] = kmeanModel.predict(dataset)
@@ -33,7 +34,7 @@ if __name__ == "__main__":
         K = range(1, 10)
         for k in K:
             kmeanModel = KMeans(n_clusters=k)
-            kmeanModel.fit(batting_performance_data[[attribute]])
+            kmeanModel.fit(batting_performance_data)
             distortions.append(kmeanModel.inertia_)
 
         plt.figure(figsize=(16, 8))
@@ -43,16 +44,14 @@ if __name__ == "__main__":
         plt.title('The Elbow Method showing the optimal k for batting performance')
         plt.show()
     else:
-        df= cluster_batting_performance(batting_performance_data[[attribute]])
+        df = cluster_batting_performance(batting_performance_data)
 
-        output_file = os.path.join(dirname, 'output\\batting_cluster.csv')
-        df.to_csv(output_file)
-        x = attribute
-        y = attribute
-
-        plt.scatter(df[x], df[y],
-                    c=df['batting_performance'])
-        plt.title("cluster visualization")
-        plt.xlabel(x)
-        plt.ylabel(y)
-        plt.show()
+        # output_file = os.path.join(dirname, 'output\\batting_cluster.csv')
+        for x in ["runs", "batting_position"]:
+            for y in batting_performance_data.columns:
+                if x != y:
+                    plt.scatter(df[x], df[y], c=df['batting_performance'])
+                    plt.title("cluster visualization")
+                    plt.xlabel(x)
+                    plt.ylabel(y)
+                    plt.show()
