12d3158bec6c702418d94beddb1fc73450c9459a
umayangag <umayangag@gmail.com>
2021-06-12 13:44:07 +0530

    weather cluster parked

--- src/analyze/cluster_weather.py
+++ src/analyze/cluster_weather.py
@@ -6,28 +6,33 @@ from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import pickle
+from config.mysql import get_db_connection
+from final_data.encoders import encode_viscosity
+from sklearn import preprocessing
 
 find_k = True
-find_k = False
+# find_k = False
 
 if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
-    filename_batting = os.path.join(dirname, 'cluster_data\\batting-weather.csv')
-    filename_bowling = os.path.join(dirname, 'cluster_data\\bowling-weather.csv')
-
-    batting_performance_data = pd.read_csv(filename_batting)
-    bowling_performance_data = pd.read_csv(filename_bowling)
-
-    print(batting_performance_data)
-    print(bowling_performance_data)
-
-    df1 = pd.concat([batting_performance_data, bowling_performance_data], ignore_index=True)
+    db_connection = get_db_connection()
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("SELECT id,temp,wind,rain,humidity,cloud,pressure,viscosity  FROM weather_data")
+    weather_list = db_cursor.fetchall()
+    df1 = pd.DataFrame(weather_list,
+                       columns=["id", "temp", "wind", "rain", "humidity", "cloud", "pressure", "viscosity"])
+    df1["viscosity"] = df1["viscosity"].apply(encode_viscosity)
+    df1 = df1.fillna(df1.mean())
+    df1 = df1.drop("id", axis=1)
+    scaler = preprocessing.StandardScaler().fit(df1)
+    data_scaled = scaler.transform(df1)
+    df1 = pd.DataFrame(data=data_scaled, columns=df1.columns)
     print(df1)
-
     if find_k:
 
         distortions = []
-        K = range(1, 10)
+        K = range(1, 50)
         for k in K:
             kmeanModel = KMeans(n_clusters=k)
             kmeanModel.fit(df1)
@@ -37,17 +42,26 @@ if __name__ == "__main__":
         plt.plot(K, distortions, 'bx-')
         plt.xlabel('k')
         plt.ylabel('Distortion')
-        plt.title('The Elbow Method showing the optimal k for batting performance')
+        plt.title('The Elbow Method showing the optimal k for weather clusters')
         plt.show()
 
     else:
 
-        kmeanModel = KMeans(n_clusters=3)
+        kmeanModel = KMeans(n_clusters=5)
         kmeanModel.fit(df1)
+        pickle.dump(kmeanModel, open("weather_cluster_model.sav", 'wb'))
+        predicted = kmeanModel.predict(df1)
+        print(predicted)
+        df1['weather'] = predicted
+        output_file = os.path.join(dirname, 'output\\weather_clusters.csv')
 
-        df1['weather'] = kmeanModel.predict(df1)
-        output_file = os.path.join(dirname, 'output\\batting_weather.csv')
-        batting_performance_data.to_csv(output_file)
+        # for row in df1.iterrows():
+        #     print(row)
+        # db_cursor.execute(f'UPDATE weather_data SET weather_category = {row[1][8]} '
+        #                   f'WHERE id = {row[0]}')
+
+        # db_connection.commit()
+        df1.to_csv(output_file)
 
         # humidity, cloud, pressure
         x = "cloud"
